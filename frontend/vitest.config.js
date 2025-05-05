b276b947c45687b87e4029f785e11031f3d1ea93
Captain Baguette <62989385+CaptainBaguettes@users.noreply.github.com>
2025-05-05 21:21:54 +0200

    [tech] Ajout CI (#12)

---
+++ frontend/vitest.config.js
@@ -0,0 +1,13 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+});
