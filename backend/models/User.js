9ff58445466b5fc00ed4c4a807ba08e532ce9225
CptBaguette <teddyvictorien.dev@gmail.com>
2025-05-05 20:35:49 +0200

    [Feat] mise en place de l'authentification et du jwt

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   mail: { type: String, required: true, unique: true },
@@ -6,8 +6,8 @@ const UserSchema = new mongoose.Schema({
   last_name: String,
   password: { type: String, required: true },
   postal_code: String,
-  profession: { type: mongoose.Schema.Types.ObjectId, ref: 'Profession' },
-  jobs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Job' }] // postule à plusieurs jobs
+  profession: { type: mongoose.Schema.Types.ObjectId, ref: "Profession" },
+  jobs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Job" }], // postule à plusieurs jobs
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
