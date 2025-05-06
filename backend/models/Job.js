11efb419c05da945bd2b16559d791473078eef24
Alexandre CAROL <sasandre10@gmail.com>
2025-05-06 12:21:46 +0200

    Add swagger

--- backend/models/Job.js
+++ backend/models/Job.js
@@ -4,7 +4,9 @@ const JobSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: String,
   city: { type: mongoose.Schema.Types.ObjectId, ref: 'City' },
-  applicants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }] // inverse de postule
+  influx: { type: Number },
+  applicants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  createdAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('Job', JobSchema);
